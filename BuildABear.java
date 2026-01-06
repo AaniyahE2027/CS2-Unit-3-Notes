2779a48f330b7078be9276712460fc18ebc6acef
Aaniyah <aaniyah.ellis2027@gbwl.org>
2026-01-06 16:06:25 +0000

    3.5

--- BuildABear.java
+++ BuildABear.java
@@ -15,7 +15,39 @@ public class BuildABear {
     //ENCAPSULATION: keep variables PRIVATE (they can't be directly accessed outside of this CLASS)
     
     //2. CONSTRUCTORS (initalize values)
-    
+    //Default constuctors does not take any arguments (no-arg)
+    //what gets called when user says "= new BuildABear"
+public BuildABear() {
+    this.type = "bear"; // WE PICK the initial values!
+    this.name = "Tony";
+    this.fillLevel = 75;
+    this.price = 150.00;
+    this.hasSpeaker = false;
+    this.recording = "";
+}
+// PARAMETERIZED CONSTRUCTOR allows user to provide args
+// user PASSES initial values to the constructor call
+public BuildABear(String t, String n, int fl) {
+    this.type = t; // USER picked the value for type!
+    this.name = n;
+    this.fillLevel = fl;
+    this.price = 150.00;
+    this.hasSpeaker = false;
+    this.recording = "";
+}
 
     //3. METHODS (behaviors/actions)
+    // ACCESSORS ("getters") return the value of an instance var.
+    public String getType() {
+        return type;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getFillLevel (){
+        return fillLevel;
+    }
+    public double getPrice(){
+        return price;
+    }
 }
